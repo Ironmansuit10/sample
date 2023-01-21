797823d3e73e722d39bfa186d8df95539de53c59
Ironmansuit10 <123226798+Ironmansuit10@users.noreply.github.com>
2023-01-21 10:45:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "11319890"))
+API_HASH = environ.get("API_HASH", "4965e574411647c42ffbd8b46040f0a5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5709769046:AAFacMwNRHjNwbnIjOY8VCrD1A3yfFqjYvE")
+ADMIN = int(environ.get("ADMIN", "1365582907"))          
+CAPTION = environ.get("CAPTION", "@Tamildubbedpublic")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
