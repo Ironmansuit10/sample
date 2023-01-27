f96219c76cd17004ac3cb066eb08adf132fe4f67
Ironmansuit10 <123226798+Ironmansuit10@users.noreply.github.com>
2023-01-27 14:59:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "11319890"))
 API_HASH = environ.get("API_HASH", "4965e574411647c42ffbd8b46040f0a5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5709769046:AAFacMwNRHjNwbnIjOY8VCrD1A3yfFqjYvE")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5977093025:AAHqridJ91UHBdu5tKhgax54j90gMkK3Y9I")
 ADMIN = int(environ.get("ADMIN", "1365582907"))          
 CAPTION = environ.get("CAPTION", "@Tamildubbedpublic")
 
